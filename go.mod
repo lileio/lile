5ce824f8a2b754ba762e86e8484d85721ac56314
Alex Barlow <a@alexb.cc>
2020-06-06 18:09:18 +0100

    First impl of protocopy!

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ require (
 	github.com/beorn7/perks v1.0.0 // indirect
 	github.com/fatih/color v1.7.0
 	github.com/gofrs/uuid v3.1.0+incompatible
-	github.com/golang/protobuf v1.3.2
+	github.com/golang/protobuf v1.4.1
 	github.com/googleapis/gax-go v2.0.2+incompatible // indirect
 	github.com/grpc-ecosystem/go-grpc-middleware v1.0.0
 	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0
@@ -18,9 +18,8 @@ require (
 	github.com/onsi/ginkgo v1.10.1 // indirect
 	github.com/onsi/gomega v1.7.0 // indirect
 	github.com/prometheus/client_golang v0.9.2
-	github.com/prometheus/client_model v0.0.0-20190129233127-fd36f4220a90 // indirect
 	github.com/prometheus/common v0.4.0 // indirect
-	github.com/rakyll/statik v0.1.7-0.20190731211841-925a23bda946
+	github.com/rakyll/statik v0.1.7
 	github.com/serenize/snaker v0.0.0-20171204205717-a683aaf2d516
 	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/cobra v0.0.3
@@ -29,7 +28,8 @@ require (
 	github.com/xlab/treeprint v0.0.0-20180616005107-d6fb6747feb6
 	github.com/xtgo/set v1.0.0
 	google.golang.org/api v0.11.0 // indirect
-	google.golang.org/grpc v1.24.0
+	google.golang.org/grpc v1.27.0
+	google.golang.org/protobuf v1.24.0
 	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
 )
 
---
+++ protocopy/protocopy.go
@@ -0,0 +1,167 @@
+package protocopy
+
+import (
+	"errors"
+	"reflect"
+	"strings"
+	"time"
+
+	"github.com/golang/protobuf/ptypes"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/runtime/protoimpl"
+)
+
+func ToProto(src, dest interface{}) error {
+	in := reflect.ValueOf(src)
+	out := reflect.ValueOf(dest)
+
+	if reflect.TypeOf(src) == reflect.TypeOf(reflect.Value{}) {
+		in = src.(reflect.Value)
+	}
+
+	if reflect.TypeOf(dest) == reflect.TypeOf(reflect.Value{}) {
+		out = dest.(reflect.Value)
+	}
+
+	return setValue(in, out)
+}
+
+func toProtoStruct(in, out reflect.Value) error {
+	if out.IsNil() {
+		out.Set(reflect.New(out.Type().Elem()))
+	}
+
+FIELD_LOOP:
+	for i := 0; i < in.NumField(); i++ {
+		field := in.Type().Field(i)
+
+		// Private fields are skipped
+		if field.PkgPath != "" {
+			continue FIELD_LOOP
+		}
+
+		inFieldName := field.Name
+
+		pbTags := strings.Split(field.Tag.Get("pb"), ",")
+		for _, v := range pbTags {
+			if v == "ignore=true" {
+				continue FIELD_LOOP
+			}
+
+			if v != "" {
+				inFieldName = strings.TrimSpace(v)
+			}
+		}
+
+		inField := in.Field(i)
+		outField := out.Elem().FieldByNameFunc(func(n string) bool {
+			return strings.EqualFold(strings.ToLower(n), strings.ToLower(inFieldName))
+		})
+
+		if !outField.IsValid() {
+			return errors.New("No field found for " + inFieldName)
+		}
+
+		err := setValue(inField, outField)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func setValue(inField, outField reflect.Value) error {
+	k := inField.Type().Kind()
+	if k == reflect.Ptr {
+		k = inField.Elem().Type().Kind()
+		inField = inField.Elem()
+	}
+
+	switch k {
+	case reflect.Array, reflect.Slice:
+		// Same types, so we can directly copy
+		if inField.Type() == outField.Type() {
+			outField.Set(inField)
+			return nil
+		}
+		outField.Set(reflect.MakeSlice(outField.Type(), inField.Len(), inField.Len()))
+		for i := 0; i < inField.Len(); i++ {
+			err := setValue(inField.Index(i), outField.Index(i))
+			if err != nil {
+				return err
+			}
+		}
+	case reflect.Struct:
+		err := toProtoStruct(inField, outField)
+		if err != nil {
+			return err
+		}
+	case reflect.Map:
+		outField.Set(reflect.MakeMap(outField.Type()))
+		for _, key := range inField.MapKeys() {
+			v := reflect.New(outField.Type().Elem()).Elem()
+			err := setValue(inField.MapIndex(key), v)
+			if err != nil {
+				return err
+			}
+
+			outField.SetMapIndex(key, v)
+		}
+	case reflect.Chan, reflect.Func, reflect.Interface:
+		return errors.New("input type not supported: " + inField.Type().Kind().String())
+	default:
+		err := setScalar(inField, outField)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+
+}
+
+func setScalar(inF, outF reflect.Value) error {
+	// We're trying to output to an ENUM
+	if outF.MethodByName("Enum").IsValid() {
+		// Deal with int enum and string
+		if inF.Kind() == reflect.String {
+			if inF.String() == "" {
+				return nil
+			}
+
+			d := protoimpl.X.EnumDescriptorOf(outF.Interface())
+			dv := d.Values().ByName(protoreflect.Name(inF.String()))
+			outF.SetInt(int64(dv.Number()))
+		} else {
+			outF.SetInt(int64(inF.Int()))
+		}
+
+		return nil
+	}
+
+	if outF.Type().String() == "*durationpb.Duration" {
+		pbd := ptypes.DurationProto(time.Duration(inF.Int()))
+		outF.Set(reflect.ValueOf(pbd))
+
+		return nil
+	}
+
+	if outF.Type().String() == "*timestamppb.Timestamp" {
+		if inF.Kind() == reflect.Ptr {
+			inF = reflect.Indirect(inF)
+		}
+
+		pbd, err := ptypes.TimestampProto(inF.Interface().(time.Time))
+		if err != nil {
+			return err
+		}
+
+		outF.Set(reflect.ValueOf(pbd))
+
+		return nil
+	}
+
+	outF.Set(inF)
+	return nil
+}
